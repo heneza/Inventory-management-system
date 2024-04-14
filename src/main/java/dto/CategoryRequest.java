fc541ee4ed7aea9eeb1df4aaf12f5c5b1d52752b
vanesa <spada.vanesa@gmail.com>
2024-04-14 14:54:10 +0200

    Merge pull request #1 from heneza/JAVA34-1-setup-maven-and-basic-repositories
    Java34 1 setup maven and basic repositories

--- src/main/java/dto/CategoryRequest.java
+++ src/main/java/dto/CategoryRequest.java
@@ -1,4 +1,19 @@
 package dto;
 
 public class CategoryRequest {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description,String name) {
+        this.description = description;
+        this.name = name;
+    }
 }
