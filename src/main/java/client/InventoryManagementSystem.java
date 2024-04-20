8b6ae3eef5451bc8b0862469c34a2e727f6b1ca0
vanesa <spada.vanesa@gmail.com>
2024-04-20 13:41:51 +0200

    Merge pull request #1 from heneza/JAVA34-1-setup-maven-and-basic-repositories
    Java34 1 setup maven and basic repositories

--- src/main/java/client/InventoryManagementSystem.java
+++ src/main/java/client/InventoryManagementSystem.java
@@ -1,4 +1,42 @@
 package client;
-
-public class InventoryManagementSystem  {
+import dto.ProductRequest;
+import service.ProductService;
+import service.ProductServiceImpl;
+import java.util.Scanner;
+public class InventoryManagementSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inventory Managment System");
+        System.out.println("1. Add product");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                createProduct(sc);
+                break;
+            case 2:
+                displayAllProduct();
+        }
     }
+    public static void createProduct(Scanner scanner) {
+        ProductRequest productRequest = new ProductRequest();
+        System.out.println("Ju lutem fusni emrin e producktit");
+        productRequest.setNa(scanner.next());
+        System.out.println("Ju lutem fusni pershkrimin e produktit");
+        productRequest.setDescription(scanner.next());
+        System.out.println("Ju lutem fusni ID e categorise");
+        productRequest.setCategoryId(scanner.nextLong());
+        System.out.println("Ju lutem fusni cmimin e produktit");
+        productRequest.setPrice(scanner.nextDouble());
+        System.out.println("Ju lutem fusni sasine e produktit");
+        productRequest.setQuantity(scanner.nextInt());
+        ProductService productService = new ProductServiceImpl();
+        productService.createProduct(productRequest);
+    }
+}
+
+
+
+
+
+
+
--- src/main/java/dto/ProductRequest.java
+++ src/main/java/dto/ProductRequest.java
@@ -5,4 +5,45 @@ public class ProductRequest {
     private String description;
     private Double price;
     private Integer quantity;
+    private Long quantityId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getQuantityId() {
+        return quantityId;
+    }
+
+    public void setQuantityId(Long quantityId) {
+        this.quantityId = quantityId;
+    }
 }
